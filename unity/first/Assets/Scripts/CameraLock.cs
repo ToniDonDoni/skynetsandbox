92bb6e2de6fcc76cfe6c82b04d76436b14ce3233
malik malik <malik@gmail.com>
2025-12-27 15:56:43 +0300

    unity

---
+++ unity/first/Assets/Scripts/CameraLock.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CameraLock : MonoBehaviour
+{
+    public float sensitivity = 5f;
+
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    void Update()
+    {
+        Quaternion deviceRotation = Input.gyro.attitude;
+        deviceRotation = new Quaternion(
+            deviceRotation.x,
+            deviceRotation.y,
+            -deviceRotation.z,
+            -deviceRotation.w
+        );
+
+        transform.localRotation = deviceRotation * Quaternion.Euler(90f, 0f, 0f);
+    }
+}
